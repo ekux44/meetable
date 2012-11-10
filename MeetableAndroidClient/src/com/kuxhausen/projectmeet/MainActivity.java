94485b2a7144f0a64bd1d50785a5296e32b6200e
ekux44 <ekux44@gmail.com>
2012-11-10 12:22:56 -0600

    Added new event button and placeholder event page

--- MeetableAndroidClient/src/com/kuxhausen/projectmeet/MainActivity.java
+++ MeetableAndroidClient/src/com/kuxhausen/projectmeet/MainActivity.java
@@ -2,14 +2,22 @@ package com.kuxhausen.projectmeet;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
-    @Override
+    Button newMeetingButton;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newMeetingButton = (Button) findViewById(R.id.newButton);
+        newMeetingButton.setOnClickListener(this);
     }
 
     @Override
@@ -17,4 +25,13 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.newButton:
+			startActivity(new Intent(this, NewMeetingActivity.class));
+			break;
+		}
+	}
 }
