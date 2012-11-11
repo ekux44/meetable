413ed3256b8bafa85e407ddcbde7c685aca041bb
ekux44 <ekux44@gmail.com>
2012-11-10 20:40:17 -0600

    Added searching&displaying contacts for invitepplple

--- MeetableAndroidClient/src/com/kuxhausen/projectmeet/InvitePeopleActivity.java
+++ MeetableAndroidClient/src/com/kuxhausen/projectmeet/InvitePeopleActivity.java
@@ -1,17 +1,24 @@
 package com.kuxhausen.projectmeet;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.CharArrayBuffer;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.QuickContactBadge;
+import android.widget.ResourceCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class InvitePeopleActivity extends Activity implements OnClickListener {
@@ -45,6 +52,15 @@ public class InvitePeopleActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_invitepeople);
 		
 		addedPeople = ((ListView)findViewById(R.id.addedPeopleListView));
+		String select = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+	             + Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+	             + Contacts.DISPLAY_NAME + " != '' ))";
+	     Cursor c =
+	             getContentResolver().query(Contacts.CONTENT_URI, CONTACTS_SUMMARY_PROJECTION, select,
+	             null, Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
+	     startManagingCursor(c);
+	     ContactListItemAdapter adapter = new ContactListItemAdapter(this, R.layout.quick_contacts, c);
+	     addedPeople.setAdapter(adapter);
 		//addedPeople.setAdapter(adapter);
 		
 		// Watch for button clicks.
@@ -86,4 +102,62 @@ public class InvitePeopleActivity extends Activity implements OnClickListener {
 	            }
 	        }
 	    }
+	 
+	 
+	 static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
+         Contacts._ID, // 0
+         Contacts.DISPLAY_NAME, // 1
+         Contacts.STARRED, // 2
+         Contacts.TIMES_CONTACTED, // 3
+         Contacts.CONTACT_PRESENCE, // 4
+         Contacts.PHOTO_ID, // 5
+         Contacts.LOOKUP_KEY, // 6
+         Contacts.HAS_PHONE_NUMBER, // 7
+ };
+
+ static final int SUMMARY_ID_COLUMN_INDEX = 0;
+ static final int SUMMARY_NAME_COLUMN_INDEX = 1;
+ static final int SUMMARY_STARRED_COLUMN_INDEX = 2;
+ static final int SUMMARY_TIMES_CONTACTED_COLUMN_INDEX = 3;
+ static final int SUMMARY_PRESENCE_STATUS_COLUMN_INDEX = 4;
+ static final int SUMMARY_PHOTO_ID_COLUMN_INDEX = 5;
+ static final int SUMMARY_LOOKUP_KEY = 6;
+ static final int SUMMARY_HAS_PHONE_COLUMN_INDEX = 7;
+
+ private final class ContactListItemAdapter extends ResourceCursorAdapter {
+     public ContactListItemAdapter(Context context, int layout, Cursor c) {
+         super(context, layout, c);
+     }
+
+     @Override
+     public void bindView(View view, Context context, Cursor cursor) {
+         final ContactListItemCache cache = (ContactListItemCache) view.getTag();
+         TextView nameView = cache.nameView;
+         QuickContactBadge photoView = cache.photoView;
+         // Set the name
+         cursor.copyStringToBuffer(SUMMARY_NAME_COLUMN_INDEX, cache.nameBuffer);
+         int size = cache.nameBuffer.sizeCopied;
+         cache.nameView.setText(cache.nameBuffer.data, 0, size);
+         final long contactId = cursor.getLong(SUMMARY_ID_COLUMN_INDEX);
+         final String lookupKey = cursor.getString(SUMMARY_LOOKUP_KEY);
+         cache.photoView.assignContactUri(Contacts.getLookupUri(contactId, lookupKey));
+     }
+
+     @Override
+     public View newView(Context context, Cursor cursor, ViewGroup parent) {
+         View view = super.newView(context, cursor, parent);
+         ContactListItemCache cache = new ContactListItemCache();
+         cache.nameView = (TextView) view.findViewById(R.id.name);
+         cache.photoView = (QuickContactBadge) view.findViewById(R.id.badge);
+         view.setTag(cache);
+
+         return view;
+     }
+ }
+
+ final static class ContactListItemCache {
+     public TextView nameView;
+     public QuickContactBadge photoView;
+     public CharArrayBuffer nameBuffer = new CharArrayBuffer(128);
+ }
 }
